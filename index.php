cc2a5429f5c44e9cf475d34d1b2feb576dde8156
Chavoosh Ghasemi <chghasemi@.cs.arizona.edu>
2019-12-11 22:04:14 +0000

    add ndn_vs_ip video & update video urls for NDN and IP

--- index.php
+++ index.php
@@ -3,17 +3,15 @@
   <head>
     <title>NDN Videos</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" href="style.css">
   </head>
 
   <body style="background: #FFF8DC">
     <img src="resource/ivisa-logo.png" style="display:block; margin-left: auto; margin-right:auto; width:30%" alt="ivisa poster">
     <h1>NDN Plays Videos!</h1>
     <h2>iViSA takes care of videos ;)</h2>
-    <a id="h1" href="html/ndn-research-questions.html">Watch Research Questions with NDN</a><br>
-    <a id="h1" href="html/ip-research-questions.html">Watch Research Questions with IP</a><br>
-    <a id="h1" href="html/ip-writing-applications.html">Watch Writing Applications with IP</a><br>
-    
+    <a id="h1" href="html/ndn-ndn_vs_ip.html">Watch NDN vs IP over NDN</a><br>
+    <a id="h1" href="html/ip-ndn_vs_ip.html">Watch NDN vs IP over IP</a><br>
+
 
     Send me mail at <a href="mailto:support@yourcompany.com">
       chghasemi@cs.arizona.edu</a>.
